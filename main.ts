8b7155b3ca4259fee456c39b4a8800f05031e3f6
capyyimg <79929439+capyyimg@users.noreply.github.com>
2021-03-08 13:33:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(number_set)
     basic.pause(200)
+    // This is when you differentiate the numbers 
     if (number_set == 0) {
         basic.showString("Z")
     } else if (number_set < 0) {
@@ -14,10 +15,11 @@ input.onButtonPressed(Button.AB, function () {
         basic.showString("P")
     }
     basic.pause(200)
+    // This is where you divide the numbers and if the number is divisible by 2 then it will show even and it it has a remainder then it will show as odd because everything odd number divided by 2 will have a remainder of 1.
     if (number_set % 2 == 1) {
-        basic.showString("Even")
-    } else {
         basic.showString("Odd")
+    } else {
+        basic.showString("Even")
     }
 })
 // When you press the A button, it will increase the variable by -1. 
